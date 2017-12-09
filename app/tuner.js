127da6ef09affe5766ae2311a1ec2525417bb3db
ddraillantclark <dd.raillantclark@gmail.com>
2017-12-09 13:21:50 -0500

    Output info on tuned station to console, and add speaker dependency

--- app/tuner.js
+++ app/tuner.js
@@ -12,15 +12,22 @@ var nowPlaying = function () {
     return currentStation;
 };
 
-
-
 module.exports.getTuner = function (app) {
 
     var tuneIn = function (station) {
+
+        if (currentStation) {
+            console.log("Tuning out " + currentStation.name);
+            lameStream.unpipe(radioSpeaker);
+            radioStream.unpipe(lameStream);
+        }
+
         currentStation = station;
-    
-        httpRequest = http.get(station.url, 
+        console.log("Tuning in " + currentStation.name);
+
+        httpRequest = http.get(currentStation.url, 
             function (response) {
+                console.log("Stream opened " + currentStation.url)
                 radioStream = response;
                 var sampleRate = 44100;
                 radioSpeaker = app.speaker({
